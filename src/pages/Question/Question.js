18a757106c64c1d984060d73f18213f126c32ed2
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2022-12-18 00:53:27 -0500

    Adding the ability upload diagrams

--- src/pages/Question/Question.js
+++ src/pages/Question/Question.js
@@ -1,38 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import { QUESTION_SCHEMA } from './QuestionSchema'
 import Markdown from '../../components/Markdown/Markdown.component'
 
 import './Question.css'
-
-const dataTemplate = {
-    questionData: {
-        question: "''",
-        optionA: "''",
-        optionB: "''",
-        optionC: "''",
-        optionD: "''",
-        optionE: "''",
-        diagram: "null",
-    },
-    solutionData: {
-        solution: "''",
-        explanation: "''",
-        diagram: "null",
-    },
-    details: {
-        grade: 0,
-        partType: "''",
-        partNumber: 0,
-        partSize: 0,
-        subject: "''",
-        topic: "''",
-    },
-    hints: {
-        hintOne: "''",
-        hintTwo: "''",
-        hintThree: "''",
-        hintFour: "''",
-    },
-}
 
 const Question = () => {
 
@@ -40,32 +10,31 @@ const Question = () => {
     const [currIdx, setCurrIdx] = useState(null)
     const [data, setData] = useState(null)
     const [menuItems, setMenuItems] = useState([])
+    const [questionDiagram, setQuestionDiagram] = useState(null)
+    const [solutionDiagram, setSolutionDiagram] = useState(null)
 
 
     useEffect(() => {
-      fetch('/questionCount')
-        .then(response => response.json())
-        .then(data => {
-            setCount(data);
-            const menuItems = [];
-            for (let i = 1; i <= data; i++) {
-              menuItems.push(i);
-            }
-            setMenuItems(menuItems);
-        });
+        fetch('/questionCount')
+            .then(response => response.json())
+            .then(data => {
+                setCount(data);
+                const menuItems = [];
+                for (let i = 1; i <= data; i++) {
+                    menuItems.push(i);
+                }
+                setMenuItems(menuItems);
+            });
     }, []);
-
-    // const menuItems = [];
-    // for (let i = 1; i <= count; i++) {
-    //     menuItems.push(i);
-    // }
 
     const onQuestionSelect = async (e) => {
         const idx = e.currentTarget.getAttribute('data-item')
-        const response = await fetch(`/getQuestion?idx=${idx-1}`);
-        const newData = await response.json();
-        setData(newData)
-        setCurrIdx(idx)
+        const response = await fetch(`/getQuestion?idx=${idx - 1}`);
+        if(response.ok){
+            const newData = await response.json();
+            setData(newData)
+            setCurrIdx(idx)    
+        }
     }
 
     const changeHandler = (e) => {
@@ -78,34 +47,102 @@ const Question = () => {
             return newData
         })
     }
+
+
+    const saveDiagram = (url, mainkey, subkey) => {
+        if (url !== null) {
+            fetch(url)
+                .then(response => response.blob())
+                .then(blob => {
+                    const formData = new FormData();
+                    formData.append('file', blob);
+
+                    return fetch('/diagrams', {
+                        method: 'POST',
+                        body: formData
+                    });
+                })
+                .then(response => response.json())
+                .then(result => {
+                    console.log(result);
+                    setData(prevData => {
+                        const newData = { ...prevData };
+                        newData[mainkey][subkey] = result.diagramID;
+                        return newData;
+                    });
+                })
+                .catch(error => console.error(error));
+        }
+    }
+
     const submitHandler = (e) => {
         e.preventDefault()
-    
+
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
         };
-    
+
+        saveDiagram(questionDiagram, 'questionData', 'diagram');
+        saveDiagram(solutionDiagram, 'solutionData', 'diagram');
+
         if (currIdx === count + 1) {
             fetch('/createQuestion', requestOptions)
             setCount(count+1)
         } else {
             fetch(`/putQuestion?idx=${currIdx-1}`, requestOptions)
         }
-        
+
     };
 
     const addQuestion = () => {
-        setData(dataTemplate);
+        setData(QUESTION_SCHEMA);
         setCurrIdx(count + 1);
         menuItems.push(count + 1);
     };
 
+    const uploadImage = (e) => {
+        const mainkey = e.target.getAttribute("mainkey")
+
+        // Check if the user is pasting an image
+        if (e.clipboardData && e.clipboardData.items) {
+            const items = e.clipboardData.items;
+            for (let i = 0; i < items.length; i++) {
+                // Check if the item is an image
+                if (items[i].type.indexOf("image") !== -1) {
+                    // Extract the image from the clipboard
+                    const blob = items[i].getAsFile();
+
+                    // Create a URL for the image
+                    const URLObj = window.URL || window.webkitURL;
+                    const source = URLObj.createObjectURL(blob);
+
+                    // Check if the label at the top of the text area has a subkey of "diagram"
+                    // If it does, set the previewImage state to the image URL
+                    if (mainkey === "questionData") {
+                        setQuestionDiagram(source);
+                    } else if (mainkey === "solutionData") {
+                        setSolutionDiagram(source)
+                    }
+
+
+                    // Insert the image into the textarea
+                    const textarea = e.target;
+                    textarea.focus();
+                    textarea.value = `![image]${source}`;
+
+                    // Stop the default paste action
+                    e.preventDefault();
+                }
+            }
+        }
+    }
+
     return (
-        <div style={{display: "flex"}}>
-            <div> 
-                <button style={{margin: "10px"}} type="button" onClick={addQuestion}>Add Question</button>
+        <div style={{ display: "flex" }}>
+            <div>
+                <button style={{ margin: "10px" }} type="button" onClick={addQuestion}>Add Question</button>
                 <div className="sidebar">
                     <ul>
                         {
@@ -118,30 +155,38 @@ const Question = () => {
             </div>
 
 
-            <div style={{margin: "10px 50px", width: "100%"}}>
+            <div style={{ margin: "10px 50px", width: "100%" }}>
                 {data === null && <p></p>}
-                { data && (
+                {data && (
                     <form onSubmit={submitHandler}>
-                    {Object.keys(data).map(key => (
-                      <div key={key}>
-                        <h3>{key}</h3>
-                        {Object.keys(data[key]).map(subKey => (
-                          <label key={subKey} style={{margin: "10px 0", display: "flex", flexDirection: "column"}}>
-                            {subKey}:
-                            {(key==="questionData" || key === "solutionData") && <Markdown>{data[key][subKey].replace(/\\\\/g, '\\')}</Markdown>}
-                            <textarea 
-                                mainkey={key}
-                                subkey={subKey}
-                                onChange={changeHandler} 
-                                style={{margin: "0 0 10px", width: "100%"}} 
-                                value={data[key][subKey]} 
-                                />
-                          </label>
+                        {Object.keys(data).map(key => (
+                            <div key={key}>
+                                <h3>{key}</h3>
+                                {Object.keys(data[key]).map(subKey => (
+                                    <label key={subKey} style={{ margin: "10px 0", display: "flex", flexDirection: "column" }}>
+                                        {subKey}:
+                                        {(key === "questionData" || key === "solutionData") && <Markdown>{data[key][subKey].replace(/\\\\/g, '\\')}</Markdown>}
+                                        {questionDiagram && key === "questionData" && subKey === "diagram" && (
+                                            <img src={questionDiagram} alt="" />
+                                        )}
+                                        {solutionDiagram && key === "solutionData" && subKey === "diagram" && (
+                                            <img src={solutionDiagram} alt="" />
+                                        )}
+
+                                        <textarea
+                                            mainkey={key}
+                                            subkey={subKey}
+                                            value={data[key][subKey]}
+                                            onChange={changeHandler}
+                                            onPaste={uploadImage}
+                                        />
+
+                                    </label>
+                                ))}
+                            </div>
                         ))}
-                      </div>
-                    ))}
-                    <button type="submit" style={{margin: "20px 0"}}>Save</button>
-                  </form>
+                        <button type="submit" style={{ margin: "20px 0" }}>Save</button>
+                    </form>
                 )}
             </div>
 
---
+++ src/pages/Question/QuestionSchema.js
@@ -0,0 +1,30 @@
+export const QUESTION_SCHEMA = {
+    questionData: {
+        question: "''",
+        optionA: "''",
+        optionB: "''",
+        optionC: "''",
+        optionD: "''",
+        optionE: "''",
+        diagram: "null",
+    },
+    solutionData: {
+        solution: "''",
+        explanation: "''",
+        diagram: "null",
+    },
+    details: {
+        grade: 0,
+        partType: "''",
+        partNumber: 0,
+        partSize: 0,
+        subject: "''",
+        topic: "''",
+    },
+    hints: {
+        hintOne: "''",
+        hintTwo: "''",
+        hintThree: "''",
+        hintFour: "''",
+    },
+}
