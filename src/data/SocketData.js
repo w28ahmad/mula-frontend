7d49760ee1ab1a65686861b0baca9feecfb0ffa1
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2022-12-23 00:51:34 -0500

    Adding timer

--- src/data/SocketData.js
+++ src/data/SocketData.js
@@ -6,6 +6,8 @@ export const SOCKET_URL = HOST+"/ws-message";
 export const CONN_RECV_TOPIC = "/topic/connect"
 export const CONN_SEND_TOPIC = "/app/connect"
 
+export const CLOSE_SEND_TOPIC = "/app/close"
+
 export const DISCONN_RECV_TOPIC = "/topic/disconnect"
 export const DISCONN_SEND_TOPIC = "/app/disconnect"
 
@@ -21,3 +23,4 @@ export const QUESTION_SET = "QUESTION_SET"
 export const USER_SOLUTION = "USER_SOLUTION"
 export const PLAYER_CONNECTION = "PLAYER_CONNECTION"
 export const PLAYER_DISCONNECTION = "PLAYER_DISCONNECTION"
+export const SESSION_CLOSE = "SESSION_CLOSE"
--- src/pages/Connect/Connect.js
+++ src/pages/Connect/Connect.js
@@ -3,8 +3,10 @@ import { useNavigate } from 'react-router-dom'
 
 import SockJsClient from '../../services/SockJsClient'
 import PlayersTable from '../../components/PlayersTable/PlayersTable.component'
-import { SOCKET_URL, CONN_RECV_TOPIC, CONN_SEND_TOPIC, 
-    DISCONN_SEND_TOPIC, PLAYER_CONNECTION, PLAYER_DISCONNECTION } from '../../data/SocketData'
+import {
+    SOCKET_URL, CONN_RECV_TOPIC, CONN_SEND_TOPIC, CLOSE_SEND_TOPIC,
+    DISCONN_SEND_TOPIC, PLAYER_CONNECTION, PLAYER_DISCONNECTION, SESSION_CLOSE
+} from '../../data/SocketData'
 
 export default function Connect() {
 
@@ -21,13 +23,19 @@ export default function Connect() {
 
     const [sessionId, setSessionId] = useState('')
 
+    const [secondsLeft, setSecondsLeft] = useState(10)
+
     const onMessage = (data) => {
-        switch(data.type) {
+        console.log(data)
+        switch (data.type) {
             case PLAYER_CONNECTION:
                 onPlayersConnect(data)
                 break
             case PLAYER_DISCONNECTION:
                 onPlayersDisconnect(data)
+                break
+            case SESSION_CLOSE:
+                startGame()
                 break
             default: break
         }
@@ -36,20 +44,20 @@ export default function Connect() {
     const onPlayersConnect = (data) => {
         setSessionId(data.sessionId)
         setPlayers(data.users)
-        if(user.id == null) setUser(data.users.at(0))
+        if (user.id == null) setUser(data.users.at(0))
     }
 
     const onPlayersDisconnect = (data) => {
         setPlayers(data.users)
     }
-    
+
     const playerBroadcast = () => clientRef.sendMessage(CONN_SEND_TOPIC, JSON.stringify(user))
-    
+
     const onConnection = () => {
         playerBroadcast()
     }
-    
-    const onDisconnect = () => {}
+
+    const onDisconnect = () => { }
 
     const removePlayer = () => {
         let data = {
@@ -61,7 +69,10 @@ export default function Connect() {
         clientRef.sendMessage(DISCONN_SEND_TOPIC, JSON.stringify(data))
     }
 
+    const closeSession = () => clientRef.sendMessage(CLOSE_SEND_TOPIC, JSON.stringify(sessionId))
+
     const startGame = () => {
+        closeSession()
         navigate("/game", {
             state: {
                 user,
@@ -73,23 +84,36 @@ export default function Connect() {
         })
     }
 
-    useEffect(()=>{
-        if(players.length >= 2) startGame()
+    useEffect(() => {
         window.onbeforeunload = () => removePlayer()
     })
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSecondsLeft((secondsLeft) => secondsLeft - 1)
+        }, 1000);
+
+        if (secondsLeft === 0) {
+            clearInterval(interval);
+            startGame();
+        }
+
+        return () => clearInterval(interval);
+    }, [secondsLeft, startGame])
 
 
     return (
         <div className="outerContainer">
             <div className="tableContainer">
-                <PlayersTable players={players}/>
+                <div>{secondsLeft} seconds left before game starts</div>
+                <PlayersTable players={players} />
                 <SockJsClient
                     url={SOCKET_URL}
                     topics={[CONN_RECV_TOPIC]}
-                    onMessage={onMessage} 
+                    onMessage={onMessage}
                     onConnect={onConnection}
                     onDisconnect={onDisconnect}
-                    ref={ (client) => { clientRef = client }} 
+                    ref={(client) => { clientRef = client }}
                 />
             </div>
         </div>
