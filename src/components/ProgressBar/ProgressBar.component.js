4228b2f43d11c4b96cd56cb1586c734f39b7a892
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2023-01-03 01:07:22 -0500

    Adding transition animation

--- src/components/ProgressBar/ProgressBar.component.js
+++ src/components/ProgressBar/ProgressBar.component.js
@@ -1,39 +1,47 @@
+import { useState, useEffect } from "react";
+
 const ProgressBar = (props) => {
-  const { bgcolor, progress, name, position } = props
+  const { bgcolor, progress, name, position } = props;
+  const [currentProgress, setCurrentProgress] = useState(progress);
 
   const containerStyles = {
     height: 20,
-    width: '100%',
+    width: "100%",
     backgroundColor: "#e0e0de",
     borderRadius: 50,
-    margin: 10
-  }
+    margin: 10,
+  };
 
   const fillerStyles = {
-    height: '100%',
-    width: `${progress}%`,
+    height: "100%",
+    width: `${currentProgress}%`,
     backgroundColor: bgcolor,
-    borderRadius: 'inherit',
-    textAlign: 'right'
-  }
+    borderRadius: "inherit",
+    textAlign: "right",
+    transition: "width 0.5s ease-in-out",
+  };
 
   const labelStyles = {
     padding: 5,
-    color: 'white',
-  }
+    color: "white",
+  };
 
   const positionStyle = {
-    fontWeight: 'bold'
-  }
+    fontWeight: "bold",
+  };
+
+  useEffect(() => {
+    setCurrentProgress(progress);
+  }, [progress]);
 
   return (
     <div style={containerStyles}>
-      <div style={fillerStyles}>
-        <span style={positionStyle}> {`${position ? position+"st" : ""}`} </span>
+      <div style={fillerStyles} key={progress}>
+        <span style={positionStyle}> {`${position ? position : ""}`} </span>
         <span style={labelStyles}>{`${name}`}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
