a1cb3d7497abd5fe02eea11c1ca89844fd3c68d1
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2023-01-04 01:01:16 -0500

    Adding a create game page

--- src/pages/CreateGame/CreateGame.js
+++ src/pages/CreateGame/CreateGame.js
@@ -12,39 +12,54 @@ export default function CreateGame() {
 
   return (
     <div className="outerContainer">
-      <div className="row create-game-container">
+      <div className="create-game-container">
         <h3 className="title">Grade</h3>
         <div className="form-group">
           <input
             type="range"
-            min="6"
+            min="7"
             max="12"
             step="1"
             className="form-control-range slider"
           />
+
+          <div className="labels">
+            <label>7</label>
+            <label>8</label>
+            <label>9</label>
+            <label>10</label>
+            <label>11</label>
+            <label>12</label>
+          </div>
         </div>
         <div className="form-group difficulty-checkboxes">
           <h3 className="title">Difficulty</h3>
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" id="easy" />
-            <label className="form-check-label" htmlFor="easy">
-              EASY
-            </label>
-          </div>
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" id="medium" />
-            <label className="form-check-label" htmlFor="medium">
-              MEDIUM
-            </label>
-          </div>
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" id="hard" />
-            <label className="form-check-label" htmlFor="hard">
-              HARD
-            </label>
+          <div className="OptionGroup">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="easy" />
+              <label className="form-check-label" htmlFor="easy">
+                EASY
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="medium" />
+              <label className="form-check-label" htmlFor="medium">
+                MEDIUM
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="hard" />
+              <label className="form-check-label" htmlFor="hard">
+                HARD
+              </label>
+            </div>
           </div>
         </div>
-        <button className="btn btn-primary submit-button">CREATE GAME</button>
+        <div className="submit-button">
+          <button className="button mt-20" style={{ width: "35%" }}>
+            CREATE GAME
+          </button>
+        </div>
       </div>
     </div>
   );
